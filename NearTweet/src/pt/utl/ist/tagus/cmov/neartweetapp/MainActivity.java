7b1f94a9d23fe2e00cb2ff151137273842a12e9c
David Dias <daviddias.p@gmail.com>
2013-04-15 05:49:15 +0100

    Fixed the "insert image by URL option" now fully works :)

--- NearTweet/src/pt/utl/ist/tagus/cmov/neartweetapp/MainActivity.java
+++ NearTweet/src/pt/utl/ist/tagus/cmov/neartweetapp/MainActivity.java
@@ -375,9 +375,13 @@ public class MainActivity extends ListActivity implements LocationListener{
 			}
 
 			if(tweet.hasImage()){
+				Toast.makeText(getApplicationContext(), "I've IMAGE", Toast.LENGTH_LONG).show();
+				
 				details.putExtra("tweet_hasImage", true);
 				details.putExtra("tweet_image", tweet.getImage());
 			}else{
+				Toast.makeText(getApplicationContext(), "I've  DONT IMAGE", Toast.LENGTH_LONG).show();
+				
 				details.putExtra("tweet_hasImage", false);
 			}
 
@@ -634,6 +638,7 @@ public class MainActivity extends ListActivity implements LocationListener{
 			HashMap<String,String> tweetInterface = new HashMap<String,String>();
 			tweetInterface.put(KEY_TEXT,text);
 			tweetInterface.put(KEY_TWEETER,username);
+			
 			tweets.add(tweetInterface);
 		}
 
